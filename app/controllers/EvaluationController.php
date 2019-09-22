90b3c3e0018666d002a6f92387a8dfb4df4ea568
arafatalam <arafat_alam@live.com>
2019-09-22 12:12:54 +0600

    graph generated

---
+++ app/controllers/EvaluationController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arafat Alam Local
+ * Date: 16-September-2019
+ * Time: 11:59
+ */
---
+++ app/views/tech_dashboard/pages/evaluation/vendorevaluation.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arafat Alam Local
+ * Date: 16-September-2019
+ * Time: 12:11
+ */
